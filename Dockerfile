c9e0b8eac78d5605ff7d7770d8a6f43c6ff1c29e
tchapi <regbasket+github@gmail.com>
2018-11-12 16:40:14 +0100

    Change tabs to spaces

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,29 @@
+# Copyright (C) Air Liquide S.A,  2017
+# Author: Sébastien Lalaurette and Cyril Ferté, La Factory, Creative Foundry
+# This file is part of Predictable Farm project.
+#
+# The MIT License (MIT)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# See the LICENSE.txt file in this repository for more information.
+
 # vim:set ft=dockerfile:
 FROM debian:jessie-backports
 
@@ -7,51 +33,51 @@ RUN groupadd -r cassandra --gid=999 && useradd -r -g cassandra --uid=999 cassand
 # grab gosu for easy step-down from root
 ENV GOSU_VERSION 1.7
 RUN set -x \
-	&& apt-get update && apt-get install -y --no-install-recommends ca-certificates wget && rm -rf /var/lib/apt/lists/* \
-	&& wget -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$(dpkg --print-architecture)" \
-	&& wget -O /usr/local/bin/gosu.asc "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$(dpkg --print-architecture).asc" \
-	&& export GNUPGHOME="$(mktemp -d)" \
-	&& gpg --keyserver ha.pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4 \
-	&& gpg --batch --verify /usr/local/bin/gosu.asc /usr/local/bin/gosu \
-	&& rm -r "$GNUPGHOME" /usr/local/bin/gosu.asc \
-	&& chmod +x /usr/local/bin/gosu \
-	&& gosu nobody true \
-	&& apt-get purge -y --auto-remove ca-certificates wget
+    && apt-get update && apt-get install -y --no-install-recommends ca-certificates wget && rm -rf /var/lib/apt/lists/* \
+    && wget -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$(dpkg --print-architecture)" \
+    && wget -O /usr/local/bin/gosu.asc "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$(dpkg --print-architecture).asc" \
+    && export GNUPGHOME="$(mktemp -d)" \
+    && gpg --keyserver ha.pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4 \
+    && gpg --batch --verify /usr/local/bin/gosu.asc /usr/local/bin/gosu \
+    && rm -r "$GNUPGHOME" /usr/local/bin/gosu.asc \
+    && chmod +x /usr/local/bin/gosu \
+    && gosu nobody true \
+    && apt-get purge -y --auto-remove ca-certificates wget
 
 # solves warning: "jemalloc shared library could not be preloaded to speed up memory allocations"
 RUN apt-get update && apt-get install -y --no-install-recommends libjemalloc1 && rm -rf /var/lib/apt/lists/*
 
 # https://github.com/docker-library/cassandra/pull/98#issuecomment-280761137
 RUN { \
-		echo 'Package: openjdk-* ca-certificates-java'; \
-		echo 'Pin: release n=*-backports'; \
-		echo 'Pin-Priority: 990'; \
-	} > /etc/apt/preferences.d/java-backports
+        echo 'Package: openjdk-* ca-certificates-java'; \
+        echo 'Pin: release n=*-backports'; \
+        echo 'Pin-Priority: 990'; \
+    } > /etc/apt/preferences.d/java-backports
 
 # https://wiki.apache.org/cassandra/DebianPackaging#Adding_Repository_Keys
 ENV GPG_KEYS \
 # gpg: key 0353B12C: public key "T Jake Luciani <jake@apache.org>" imported
-	514A2AD631A57A16DD0047EC749D6EEC0353B12C \
+    514A2AD631A57A16DD0047EC749D6EEC0353B12C \
 # gpg: key FE4B2BDA: public key "Michael Shuler <michael@pbandjelly.org>" imported
-	A26E528B271F19B9E5D8E19EA278B781FE4B2BDA
+    A26E528B271F19B9E5D8E19EA278B781FE4B2BDA
 RUN set -ex; \
-	export GNUPGHOME="$(mktemp -d)"; \
-	for key in $GPG_KEYS; do \
-		gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"; \
-	done; \
-	gpg --export $GPG_KEYS > /etc/apt/trusted.gpg.d/cassandra.gpg; \
-	rm -r "$GNUPGHOME"; \
-	apt-key list
+    export GNUPGHOME="$(mktemp -d)"; \
+    for key in $GPG_KEYS; do \
+        gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"; \
+    done; \
+    gpg --export $GPG_KEYS > /etc/apt/trusted.gpg.d/cassandra.gpg; \
+    rm -r "$GNUPGHOME"; \
+    apt-key list
 
 RUN echo 'deb http://www.apache.org/dist/cassandra/debian 311x main' >> /etc/apt/sources.list.d/cassandra.list
 
 ENV CASSANDRA_VERSION 3.11.0
 
 RUN apt-get update \
-	&& apt-get install -y \
-		cassandra \
-		cassandra-tools \
-	&& rm -rf /var/lib/apt/lists/*
+    && apt-get install -y \
+        cassandra \
+        cassandra-tools \
+    && rm -rf /var/lib/apt/lists/*
 
 # https://issues.apache.org/jira/browse/CASSANDRA-11661
 RUN sed -ri 's/^(JVM_PATCH_VERSION)=.*/\1=25/' /etc/cassandra/cassandra-env.sh
@@ -62,8 +88,8 @@ COPY docker-entrypoint.sh /docker-entrypoint.sh
 ENTRYPOINT ["/docker-entrypoint.sh"]
 
 RUN mkdir -p /var/lib/cassandra "$CASSANDRA_CONFIG" \
-	&& chown -R cassandra:cassandra /var/lib/cassandra "$CASSANDRA_CONFIG" \
-	&& chmod 777 /var/lib/cassandra "$CASSANDRA_CONFIG"
+    && chown -R cassandra:cassandra /var/lib/cassandra "$CASSANDRA_CONFIG" \
+    && chmod 777 /var/lib/cassandra "$CASSANDRA_CONFIG"
 VOLUME /var/lib/cassandra
 
 # 7000: intra-node communication
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,131 +1,127 @@
 #!/bin/bash
+
+# Copyright (C) Air Liquide S.A,  2017
+# Author: Sébastien Lalaurette and Cyril Ferté, La Factory, Creative Foundry
+# This file is part of Predictable Farm project.
+#
+# The MIT License (MIT)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# See the LICENSE.txt file in this repository for more information.
+
 set -e
 
 {
-	LOCKFILE=/var/tmp/lock.txt
-	#if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`; then
-	if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`; then
-    	echo "already running"
-    	exit
-	fi
-	# make sure the lockfile is removed when we exit and then claim it
-	trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
-	echo $$ > ${LOCKFILE}
+    LOCKFILE=/var/tmp/lock.txt
+    #if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`; then
+    if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`; then
+        echo "already running"
+        exit
+    fi
+    # make sure the lockfile is removed when we exit and then claim it
+    trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
+    echo $$ > ${LOCKFILE}
 
 
-	{
-	  while ! echo -n > /dev/tcp/localhost/9042; do
-	    sleep 10
-	  done
-	} 2>/dev/null
+    {
+        while ! echo -n > /dev/tcp/localhost/9042; do
+            sleep 10
+        done
+    } 2>/dev/null
 
 
-	if [ ! -d "/var/lib/cassandra/data/predictablefarm" ]; then
-		echo "Initializing Cassandra database"
-		cqlsh -e "DROP KEYSPACE IF EXISTS PredictableFarm"
-		cqlsh -e "CREATE KEYSPACE PredictableFarm  WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 }"
-		cqlsh -k predictablefarm -e "CREATE TABLE sensorlog (device_id varchar,sensor_type varchar,sensor_value varchar, created_at timestamp, PRIMARY KEY ((device_id, sensor_type),  created_at))"
-		cqlsh -k predictablefarm -e "CREATE TABLE relaystate (device_id varchar,sensor_type varchar,sensor_value int,last_update timestamp, PRIMARY KEY ((device_id, sensor_type)))"
-		cqlsh -k predictablefarm -e "CREATE TABLE zone (id_zone int PRIMARY KEY, name varchar,location varchar,location_gps varchar, dashboards text)"
-		cqlsh -k predictablefarm -e "CREATE TABLE probe (id_probe int PRIMARY KEY,id_zone int,name varchar,uuid varchar)"
-		cqlsh -k predictablefarm -e "CREATE TABLE reading (id_sensor int PRIMARY KEY,value varchar,time timestamp)"
-		cqlsh -k predictablefarm -e "CREATE TABLE sensor (id_sensor int PRIMARY KEY,id_probe int,type varchar,last_value varchar,last_time timestamp,sort_order int)"
+    if [ ! -d "/var/lib/cassandra/data/predictablefarm" ]; then
+        echo "Initializing Cassandra database"
+        cqlsh -e "DROP KEYSPACE IF EXISTS PredictableFarm"
+        cqlsh -e "CREATE KEYSPACE PredictableFarm WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 }"
+        cqlsh -k predictablefarm -e "CREATE TABLE sensorlog (device_id varchar,sensor_type varchar,sensor_value varchar, created_at timestamp, PRIMARY KEY ((device_id, sensor_type),  created_at))"
+        cqlsh -k predictablefarm -e "CREATE TABLE relaystate (device_id varchar,sensor_type varchar,sensor_value int,last_update timestamp, PRIMARY KEY ((device_id, sensor_type)))"
+        cqlsh -k predictablefarm -e "CREATE TABLE zone (id_zone int PRIMARY KEY, name varchar,location varchar,location_gps varchar, dashboards text)"
+        cqlsh -k predictablefarm -e "CREATE TABLE probe (id_probe int PRIMARY KEY,id_zone int,name varchar,uuid varchar)"
+        cqlsh -k predictablefarm -e "CREATE TABLE reading (id_sensor int PRIMARY KEY,value varchar,time timestamp)"
+        cqlsh -k predictablefarm -e "CREATE TABLE sensor (id_sensor int PRIMARY KEY,id_probe int,type varchar,last_value varchar,last_time timestamp,sort_order int)"
 
-  	# Control will enter here if $DIRECTORY doesn't exist.
-		cqlsh -k predictablefarm -e "INSERT INTO probe (id_probe, id_zone, name, uuid) VALUES (1,1,'Arduino','ed6f47ad4fba493085d4f9fdeb3f6a75')"
-		cqlsh -k predictablefarm -e "INSERT INTO probe (id_probe, id_zone, name, uuid) VALUES (2,0,'BT proto','ebf22bd24ec34ac2bc2618e21f71f753')"
-		cqlsh -k predictablefarm -e "INSERT INTO probe (id_probe, id_zone, name, uuid) VALUES (173,0,'Ethernet proto','3735928559')"
-		cqlsh -k predictablefarm -e "INSERT INTO reading (id_sensor,value,time ) VALUES (1,'20.5','2016-11-23 10:06:39')"
+        echo "Cassandra database initialized :)"
+    fi
 
-		cqlsh -k predictablefarm -e "INSERT INTO sensor (id_sensor, id_probe, type, last_value, last_time, sort_order) VALUES (1,1,'temperature','23.7','2016-11-28 17:37:03',NULL)"
-		cqlsh -k predictablefarm -e "INSERT INTO sensor (id_sensor, id_probe, type, last_value, last_time, sort_order) VALUES (2,1,'pressure','102118','2016-11-28 17:38:32',NULL)"
-		cqlsh -k predictablefarm -e "INSERT INTO sensor (id_sensor, id_probe, type, last_value, last_time, sort_order) VALUES (3,1,'lux','343','2016-11-28 17:38:32',NULL)"
-		cqlsh -k predictablefarm -e "INSERT INTO sensor (id_sensor, id_probe, type, last_value, last_time, sort_order) VALUES (4,2,'sunuvlight','4','2017-02-06 16:49:25',1)"
-		cqlsh -k predictablefarm -e "INSERT INTO sensor (id_sensor, id_probe, type, last_value, last_time, sort_order) VALUES (5,2,'lux','274','2017-02-06 16:49:37',2)"
-		cqlsh -k predictablefarm -e "INSERT INTO sensor (id_sensor, id_probe, type, last_value, last_time, sort_order) VALUES (6,2,'sunvisiblelight','264','2017-02-06 16:49:31',3)"
-		cqlsh -k predictablefarm -e "INSERT INTO sensor (id_sensor, id_probe, type, last_value, last_time, sort_order) VALUES (7,2,'sunirlight','269','2017-02-06 16:49:37',0)"
-		cqlsh -k predictablefarm -e "INSERT INTO sensor (id_sensor, id_probe, type, last_value, last_time, sort_order) VALUES (8,2,'humidity','38.5','2017-02-06 16:49:01',5)"
-		cqlsh -k predictablefarm -e "INSERT INTO sensor (id_sensor, id_probe, type, last_value, last_time, sort_order) VALUES (9,2,'pressure','101379','2017-02-06 16:49:39',6)"
-		cqlsh -k predictablefarm -e "INSERT INTO sensor (id_sensor, id_probe, type, last_value, last_time, sort_order) VALUES (10,3,'temperature','20.3','2017-02-06 16:49:33',4)"
-
-
-
-		# cqlsh -k predictablefarm -e "INSERT INTO zone (id_zone, name, location, location_gps, dashboards) VALUES (0, 'Zone par defaut',NULL,NULL,\$\$[{'name':'Heat / Moisture','blocks':[{'type':'sensors','name':'Heat / Moisture','sensors':[{'id_sensor':'8','id_probe':'2','type':'humidity','last_value':'42.7','last_time':'2017-02-02 15:28:13','sort_order':5,'probe_uuid':'ebf22bd24ec34ac2bc2618e21f71f753','probe_name':'BT proto','id_zone':'0','label':'Humidite','style':'percent','class':'air','color':'red','sensor_index':0},{'id_sensor':'10','id_probe':'2','type':'temperature','last_value':'21.1','last_time':'2017-02-02 15:28:06','sort_order':4,'probe_uuid':'ebf22bd24ec34ac2bc2618e21f71f753','probe_name':'BT proto','id_zone':'0','label':'Temperature','style':'celcius-degrees','class':'air','color':'red','sensor_index':1},{'id_sensor':'4','id_probe':'2','type':'sunuvlight','last_value':'5','last_time':'2017-02-02 15:28:20','sort_order':1,'probe_uuid':'ebf22bd24ec34ac2bc2618e21f71f753','probe_name':'BT proto','id_zone':'0','label':'UV (Ultraviolet)','style':'','class':'light','color':'red','sensor_index':2}],'displayChart':true,'displaySensor':true,'id_zone':'0','dashboard_index':0,'block_index':0,'sensor_ids':'8,10,4'}]},{'name':'Pressure / Light','blocks':[{'type':'sensors','name':'Pressure / Light','sensors':[{'id_sensor':'9','id_probe':'2','type':'pressure','last_value':'99657','last_time':'2017-02-02 15:28:20','sort_order':6,'probe_uuid':'ebf22bd24ec34ac2bc2618e21f71f753','probe_name':'BT proto','id_zone':'0','label':'Pression Atmospherique','style':'pascal-to-hectopascal','class':'air','color':'red','sensor_index':0},{'id_sensor':'5','id_probe':'2','type':'lux','last_value':'438','last_time':'2017-02-02 15:28:14','sort_order':2,'probe_uuid':'ebf22bd24ec34ac2bc2618e21f71f753','probe_name':'BT proto','id_zone':'0','label':'Luminosite','style':'lux','class':'light','color':'red','sensor_index':1},{'id_sensor':'10','id_probe':'2','type':'temperature','last_value':'21.1','last_time':'2017-02-02 15:28:06','sort_order':4,'probe_uuid':'ebf22bd24ec34ac2bc2618e21f71f753','probe_name':'BT proto','id_zone':'0','label':'Temperature','style':'celcius-degrees','class':'air','color':'red','sensor_index':2}],'displayChart':true,'displaySensor':true,'id_zone':'0','dashboard_index':1,'block_index':0,'sensor_ids':'9,5,10'}]}]\$\$)"
-		#
-		# cqlsh -k predictablefarm -e "INSERT INTO sensor (id_sensor, id_probe, type, last_value, last_time, sort_order) VALUES (11, 0, 'light', '42', toTimestamp(now()),0)"
-		#
-		# echo "Cassandra database initialized"
-
-		# cqlsh:predictablefarm> INSERT INTO probe (id_probe, id_zone, name, uuid) VALUES (0,0,'Device 1','1') ;
-		# cqlsh:predictablefarm> INSERT INTO probe (id_probe, id_zone, name, uuid) VALUES (1,0,'Device 2','2') ;
-		cqlsh -k predictablefarm -e "INSERT INTO zone (id_zone, name, location, location_gps, dashboards) VALUES (0, 'Zone par defaut 2',NULL,NULL,\$\$[{\"name\":\"Heat2 / Moisture\",\"blocks\":[{\"type\":\"sensors\",\"name\":\"Heat2 / Moisture\",\"sensors\":[{\"id_sensor\":\"8\",\"id_probe\":\"2\",\"type\":\"humidity\",\"last_value\":\"42.7\",\"last_time\":\"2017-02-02 15:28:13\",\"sort_order\":5,\"probe_uuid\":\"ebf22bd24ec34ac2bc2618e21f71f753\",\"probe_name\":\"BT proto\",\"id_zone\":\"0\",\"label\":\"Humidite\",\"style\":\"percent\",\"class\":\"air\",\"color\":\"red\",\"sensor_index\":0},{\"id_sensor\":\"10\",\"id_probe\":\"2\",\"type\":\"temperature\",\"last_value\":\"21.1\",\"last_time\":\"2017-02-02 15:28:06\",\"sort_order\":4,\"probe_uuid\":\"ebf22bd24ec34ac2bc2618e21f71f753\",\"probe_name\":\"BT proto\",\"id_zone\":\"0\",\"label\":\"Temperature\",\"style\":\"celcius-degrees\",\"class\":\"air\",\"color\":\"red\",\"sensor_index\":1},{\"id_sensor\":\"4\",\"id_probe\":\"2\",\"type\":\"sunuvlight\",\"last_value\":\"5\",\"last_time\":\"2017-02-02 15:28:20\",\"sort_order\":1,\"probe_uuid\":\"ebf22bd24ec34ac2bc2618e21f71f753\",\"probe_name\":\"BT proto\",\"id_zone\":\"0\",\"label\":\"UV (Ultraviolet)\",\"style\":\"\",\"class\":\"light\",\"color\":\"red\",\"sensor_index\":2}],\"displayChart\":true,\"displaySensor\":true,\"id_zone\":\"0\",\"dashboard_index\":0,\"block_index\":0,\"sensor_ids\":\"8,10,4\"}]},{\"name\":\"Pressure2 / Light\",\"blocks\":[{\"type\":\"sensors\",\"name\":\"Pressure / Light\",\"sensors\":[{\"id_sensor\":\"9\",\"id_probe\":\"2\",\"type\":\"pressure\",\"last_value\":\"99657\",\"last_time\":\"2017-02-02 15:28:20\",\"sort_order\":6,\"probe_uuid\":\"ebf22bd24ec34ac2bc2618e21f71f753\",\"probe_name\":\"BT proto\",\"id_zone\":\"0\",\"label\":\"Pression Atmospherique\",\"style\":\"pascal-to-hectopascal\",\"class\":\"air\",\"color\":\"red\",\"sensor_index\":0},{\"id_sensor\":\"5\",\"id_probe\":\"2\",\"type\":\"lux\",\"last_value\":\"438\",\"last_time\":\"2017-02-02 15:28:14\",\"sort_order\":2,\"probe_uuid\":\"ebf22bd24ec34ac2bc2618e21f71f753\",\"probe_name\":\"BT proto\",\"id_zone\":\"0\",\"label\":\"Luminosite\",\"style\":\"lux\",\"class\":\"light\",\"color\":\"red\",\"sensor_index\":1},{\"id_sensor\":\"10\",\"id_probe\":\"2\",\"type\":\"temperature\",\"last_value\":\"21.1\",\"last_time\":\"2017-02-02 15:28:06\",\"sort_order\":4,\"probe_uuid\":\"ebf22bd24ec34ac2bc2618e21f71f753\",\"probe_name\":\"BT proto\",\"id_zone\":\"0\",\"label\":\"Temperature\",\"style\":\"celcius-degrees\",\"class\":\"air\",\"color\":\"red\",\"sensor_index\":2}],\"displayChart\":true,\"displaySensor\":true,\"id_zone\":\"0\",\"dashboard_index\":1,\"block_index\":0,\"sensor_ids\":\"9,5,10\"}]}]\$\$)"
-
-		cqlsh -k predictablefarm -e "INSERT INTO sensor (id_sensor, id_probe, type, last_value, last_time, sort_order) VALUES (11, 0, 'light', '42', toTimestamp(now()),0)"
-
-		echo "Cassandra database initialized :)"
-	fi
-
-	rm -f ${LOCKFILE}
+    rm -f ${LOCKFILE}
 }&
 
 
 # first arg is `-f` or `--some-option`
 if [ "${1:0:1}" = '-' ]; then
-	set -- cassandra -f "$@"
+    set -- cassandra -f "$@"
 fi
 
 # allow the container to be started with `--user`
 if [ "$1" = 'cassandra' -a "$(id -u)" = '0' ]; then
-	chown -R cassandra /var/lib/cassandra /var/log/cassandra "$CASSANDRA_CONFIG"
-	exec gosu cassandra "$BASH_SOURCE" "$@"
+    chown -R cassandra /var/lib/cassandra /var/log/cassandra "$CASSANDRA_CONFIG"
+    exec gosu cassandra "$BASH_SOURCE" "$@"
 fi
 
 if [ "$1" = 'cassandra' ]; then
-	: ${CASSANDRA_RPC_ADDRESS='0.0.0.0'}
+    : ${CASSANDRA_RPC_ADDRESS='0.0.0.0'}
 
-	: ${CASSANDRA_LISTEN_ADDRESS='auto'}
-	if [ "$CASSANDRA_LISTEN_ADDRESS" = 'auto' ]; then
-		CASSANDRA_LISTEN_ADDRESS="$(hostname --ip-address)"
-	fi
+    : ${CASSANDRA_LISTEN_ADDRESS='auto'}
+    if [ "$CASSANDRA_LISTEN_ADDRESS" = 'auto' ]; then
+        CASSANDRA_LISTEN_ADDRESS="$(hostname --ip-address)"
+    fi
 
-	: ${CASSANDRA_BROADCAST_ADDRESS="$CASSANDRA_LISTEN_ADDRESS"}
+    : ${CASSANDRA_BROADCAST_ADDRESS="$CASSANDRA_LISTEN_ADDRESS"}
 
-	if [ "$CASSANDRA_BROADCAST_ADDRESS" = 'auto' ]; then
-		CASSANDRA_BROADCAST_ADDRESS="$(hostname --ip-address)"
-	fi
-	: ${CASSANDRA_BROADCAST_RPC_ADDRESS:=$CASSANDRA_BROADCAST_ADDRESS}
+    if [ "$CASSANDRA_BROADCAST_ADDRESS" = 'auto' ]; then
+        CASSANDRA_BROADCAST_ADDRESS="$(hostname --ip-address)"
+    fi
+    : ${CASSANDRA_BROADCAST_RPC_ADDRESS:=$CASSANDRA_BROADCAST_ADDRESS}
 
-	if [ -n "${CASSANDRA_NAME:+1}" ]; then
-		: ${CASSANDRA_SEEDS:="cassandra"}
-	fi
-	: ${CASSANDRA_SEEDS:="$CASSANDRA_BROADCAST_ADDRESS"}
+    if [ -n "${CASSANDRA_NAME:+1}" ]; then
+        : ${CASSANDRA_SEEDS:="cassandra"}
+    fi
+    : ${CASSANDRA_SEEDS:="$CASSANDRA_BROADCAST_ADDRESS"}
 
-	sed -ri 's/(- seeds:).*/\1 "'"$CASSANDRA_SEEDS"'"/' "$CASSANDRA_CONFIG/cassandra.yaml"
+    sed -ri 's/(- seeds:).*/\1 "'"$CASSANDRA_SEEDS"'"/' "$CASSANDRA_CONFIG/cassandra.yaml"
 
-	for yaml in \
-		broadcast_address \
-		broadcast_rpc_address \
-		cluster_name \
-		endpoint_snitch \
-		listen_address \
-		num_tokens \
-		rpc_address \
-		start_rpc \
-	; do
-		var="CASSANDRA_${yaml^^}"
-		val="${!var}"
-		if [ "$val" ]; then
-			sed -ri 's/^(# )?('"$yaml"':).*/\2 '"$val"'/' "$CASSANDRA_CONFIG/cassandra.yaml"
-		fi
-	done
+    for yaml in \
+        broadcast_address \
+        broadcast_rpc_address \
+        cluster_name \
+        endpoint_snitch \
+        listen_address \
+        num_tokens \
+        rpc_address \
+        start_rpc \
+    ; do
+        var="CASSANDRA_${yaml^^}"
+        val="${!var}"
+        if [ "$val" ]; then
+            sed -ri 's/^(# )?('"$yaml"':).*/\2 '"$val"'/' "$CASSANDRA_CONFIG/cassandra.yaml"
+        fi
+    done
 
-	for rackdc in dc rack; do
-		var="CASSANDRA_${rackdc^^}"
-		val="${!var}"
-		if [ "$val" ]; then
-			sed -ri 's/^('"$rackdc"'=).*/\1 '"$val"'/' "$CASSANDRA_CONFIG/cassandra-rackdc.properties"
-		fi
-	done
+    for rackdc in dc rack; do
+        var="CASSANDRA_${rackdc^^}"
+        val="${!var}"
+        if [ "$val" ]; then
+            sed -ri 's/^('"$rackdc"'=).*/\1 '"$val"'/' "$CASSANDRA_CONFIG/cassandra-rackdc.properties"
+        fi
+    done
 fi
 
 exec "$@"
